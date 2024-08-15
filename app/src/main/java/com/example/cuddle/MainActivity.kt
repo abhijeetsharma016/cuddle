3d743ee01007188366c461a3c8f1a438920618cc
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-08-16 01:01:08 +0530

    set up bottom nav on the app

--- app/src/main/java/com/example/cuddle/MainActivity.kt
+++ app/src/main/java/com/example/cuddle/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.cuddle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }

f0283af6eec01e7046655bb7db5d825c7c244dbf
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-09-20 12:11:28 +0530

    created message model data class

---
+++ app/src/main/java/com/example/cuddle/model/messageModel.kt
@@ -0,0 +1,8 @@
+package com.example.cuddle.model
+
+data class messageModel(
+    val senderId: String? = null,
+    val message: String? = null,
+    val currentTime: Long? = null,
+    val currentDate: String? = null
+)
